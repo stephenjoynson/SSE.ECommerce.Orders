c03c3eda1430c88def1fa3c2c1a41b62a8ae5344
Stephen Joynson <stephen.joynson@mmtdigital.co.uk>
2021-03-06 12:23:37 +0000

    Add project files.

---
+++ SSE.ECommerce.Orders/Controllers/OrdersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SSE.ECommerce.Orders.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrdersController : ControllerBase
+    {
+        private readonly ILogger<OrdersController> _logger;
+
+        public OrdersController(ILogger<OrdersController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            _logger.LogInformation("Start of Get()");
+            return string.Empty;
+        }
+    }
+}
